c59470ef3c894a1581699b11cbb03519985d3670
Adam T. Gautier <adam@gautier.org>
2022-11-22 10:01:04 -0500

    updated the append configmap script

--- append-configmap.py
+++ append-configmap.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+#
+# append-configmap: Indents the dashy conf.yml file so it can be concatenated to
+#                   the `manifest/configmap.yml`. to use
+#  `cat manifest/conf.yml | python3 append-configmap.py >> manifext/configmap.yml`
 import sys
  
 for line in sys.stdin:
